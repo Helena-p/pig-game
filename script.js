b51e533e34cc56b8e090bd7972324a461fdf7f9a
Helena <helenasleterkesto@gmail.com>
2021-04-30 10:02:29 +0200

    Add current score function to  update and increment players current score

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // SELECTING ELEMENTS
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +15,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 //ROLLING DICE FUNCTIONALITY
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -22,5 +26,12 @@ btnRoll.addEventListener('click', function () {
   diceEl.classList.remove('hidden');
   // show dice img to dice nr
   diceEl.src = `dice-${dice}.png`;
-  // 3. Check for rolled 1: if true, switch to next player
+  // 3. Check for rolled 1: if true
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // TODO: change player
+  } else {
+    // switch to next player
+  }
 });
