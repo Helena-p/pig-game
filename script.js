5642d6b323ae595e99a10daab056198dc0ad1587
Helena <helenasleterkesto@gmail.com>
2021-04-30 14:42:10 +0200

    Add function to initiate game. Refactoring

--- script.js
+++ script.js
@@ -12,16 +12,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 // STARTING CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-let playing = true;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 //SWITCH PLAYER FUNCTION
 const switchPlayer = function () {
@@ -69,6 +80,7 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 20) {
       // finish game
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -81,3 +93,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// RESETTING THE GAME
+btnNew.addEventListener('click', init);
